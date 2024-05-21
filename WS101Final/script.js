d9ee80766c1b2003f73a20ce64e4c6252171e4c4
Lieeee21 <144812284+Lieeee21@users.noreply.github.com>
2024-05-21 18:55:58 +0800

    Delete WS101Final directory

--- WS101Final/script.js
+++
@@ -1,9 +0,0 @@
-const btnContainer = document.querySelector('ul');
-const btns = document.querySelector('li a');
-
-btnContainer.forEach(liAactive => {
-    liAactive.addEventListener(`click`, function () {
-        btnContainer.querySelector(`.active`).classList.remove(` active`)
-        liAactive.classList.add(`active`)
-    })
-})
